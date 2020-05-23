e21e769251fcea8a7ff1b4f6b0689ac8386e9520
Idir Ouhab Meskine <ouhab.idir@gmail.com>
2020-05-23 02:03:15 +0200

    cdn

--- src/providers/image.jsx
+++ src/providers/image.jsx
@@ -1,15 +1,15 @@
 import axios from "axios";
-import {backendUrl} from "../variables/general";
+import {imageBackendUrl} from "../variables/general";
 import AuthService from "../services/auth";
 
 const ImageProvider = {
     saveImage: async function (image) {
         const formData = new FormData();
         formData.append("image", image);
-        return await axios.post(`${backendUrl}/api/image`, formData, {headers: AuthService.authHeader()})
+        return await axios.post(`${imageBackendUrl}/api/image`, formData, {headers: AuthService.authHeader()})
     },
     deleteImage: async function (imageId) {
-        return await axios.delete(`${backendUrl}/api/image/${imageId}`, {headers: AuthService.authHeader()})
+        return await axios.delete(`${imageBackendUrl}/api/image/${imageId}`, {headers: AuthService.authHeader()})
     },
 };
 
--- src/variables/general.jsx
+++ src/variables/general.jsx
@@ -2,7 +2,8 @@ import i18n from "../i18n";
 
 const drawerWidth = 240;
 const backendUrl = process.env.REACT_APP_ENV_BACKEND_URL;
-const imageUrl = backendUrl + '/uploads/';
+const imageBackendUrl = process.env.REACT_APP_ENV_IMAGE_BACKEND_URL;
+const imageUrl = imageBackendUrl + '/uploads/';
 const DEFAULT_QUESTION_TYPE = 'MULTIPLE_CHOICE';
 const QUESTION_TYPES = {
     FREE_TEXT: 'free_text',
@@ -10,4 +11,4 @@ const QUESTION_TYPES = {
 };
 const LANGUAGE = i18n.language.split('-')[0];
 
-export {drawerWidth, backendUrl, imageUrl, DEFAULT_QUESTION_TYPE, QUESTION_TYPES, LANGUAGE};
+export {drawerWidth, backendUrl, imageUrl, DEFAULT_QUESTION_TYPE, QUESTION_TYPES, LANGUAGE, imageBackendUrl};
