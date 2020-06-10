987a2a070dbf894afdd1b65e74abb1071a67e106
Idir Ouhab Meskine <ouhab.idir@gmail.com>
2020-06-10 22:49:08 +0200

    add responsive

--- src/components/ButtonAppBar/ButtonAppBar.jsx
+++ src/components/ButtonAppBar/ButtonAppBar.jsx
@@ -5,7 +5,6 @@ import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import { drawerWidth, LANGUAGE, LANGUAGES_LABEL } from "../../variables/general";
-import routes from "../../routes";
 import Button from "@material-ui/core/Button";
 import { useTranslation } from "react-i18next";
 import Auth from "../../providers/auth";
@@ -13,10 +12,10 @@ import TranslateIcon from "@material-ui/icons/Translate";
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
 import { ExitToApp, NoteAdd } from "@material-ui/icons";
-import green from "@material-ui/core/colors/green";
 import Tooltip from "@material-ui/core/Tooltip";
 import Link from "@material-ui/core/Link";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import MoreIcon from "@material-ui/icons/MoreVert";
 
 const useStyles = makeStyles((theme) => (
   {
@@ -26,10 +25,29 @@ const useStyles = makeStyles((theme) => (
     grow: {
       flex: "1 1 auto",
     },
+    sectionDesktop: {
+      display: "none",
+      [theme.breakpoints.up("md")]: {
+        display: "flex",
+      },
+    },
+    sectionMobile: {
+      display: "flex",
+      [theme.breakpoints.up("md")]: {
+        display: "none",
+      },
+    },
     language: {
       margin: theme.spacing(0, 0.5, 0, 1),
       display: "none",
       [theme.breakpoints.up("md")]: {
+        display: "block",
+      },
+    },
+    languageMobile: {
+      margin: theme.spacing(0, 0.5, 0, 1),
+      display: "none",
+      [theme.breakpoints.down("md")]: {
         display: "block",
       },
     },
@@ -54,23 +72,128 @@ export default function ButtonAppBar (props) {
   const classes = useStyles();
   const { t } = useTranslation("common");
   const [anchorEl, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+  const isMenuOpen = Boolean(anchorEl);
+
   const logout = () => {
     Auth.logout();
     window.location.reload();
   };
 
-  const handleClick = (event) => {
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const handleProfileMenuOpen = (event) => {
     setAnchorEl(event.currentTarget);
   };
 
-  const handleClose = () => {
+  const handleMenuClose = () => {
     setAnchorEl(null);
   };
 
+  const menuId = "primary-search-account-menu";
+  const renderLanguageMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+      PaperProps={{
+        style: {
+          maxHeight: ITEM_HEIGHT * 4.5,
+          width: "20ch",
+        },
+      }}
+    >
+      {LANGUAGES_LABEL.map((language) => (
+        <MenuItem key={language.code}
+                  component={Link}
+                  data-no-link="true"
+                  href={`?lng=${language.code}`}
+                  selected={language.code === LANGUAGE}
+                  lang={language.code}
+                  hrefLang={language.code}
+                  onClick={handleMenuClose}>
+          {language.text}
+        </MenuItem>
+      ))}
+    </Menu>
+  );
+
+  const mobileMenuId = "primary-search-account-menu-mobile";
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem onClick={handleProfileMenuOpen}>
+        <Tooltip title={t("change_language")} enterDelay={300}>
+          <Button
+            color="inherit"
+            onClick={handleProfileMenuOpen}
+          >
+            <TranslateIcon/>
+
+            <span className={classes.languageMobile}>
+              {LANGUAGES_LABEL.filter((language) => {
+                return language.code === LANGUAGE;
+              })[0].text}
+              </span>
+            <ExpandMoreIcon fontSize="small"/>
+          </Button>
+        </Tooltip>
+
+      </MenuItem>
+      <MenuItem>
+        <Tooltip title={t("sections.create-exam")} enterDelay={300}>
+          <IconButton
+            color="inherit"
+            component={Link}
+
+            href={"create-exam"}
+          >
+            <NoteAdd/>
+          </IconButton>
+        </Tooltip>
+        <p>{t("sections.create-exam")}</p>
+      </MenuItem>
+      <MenuItem>
+        <Tooltip title={t("logout")} enterDelay={300}>
+          <IconButton
+            color="inherit"
+            onClick={logout}
+          >
+            <ExitToApp/>
+          </IconButton>
+        </Tooltip>
+        <p>{t("logout")}</p>
+      </MenuItem>
+    </Menu>
+  );
+
   return (
     <div className={classes.root}>
+
+
       <AppBar elevation={1} position="fixed" className={classes.appBar}>
         <Toolbar>
+          <div style={{ backgroundColor: isMobileMenuOpen ? "red" : "blue" }}>
+            {JSON.stringify(!!mobileMoreAnchorEl)}
+          </div>
           <IconButton
             edge="start"
             onClick={props.handleDrawerToggle}
@@ -80,65 +203,58 @@ export default function ButtonAppBar (props) {
           </IconButton>
           <div className={classes.grow}/>
 
-          <Tooltip title={t("change_language")} enterDelay={300}>
-            <Button
-              color="inherit"
-              onClick={handleClick}
-            >
-              <TranslateIcon/>
-              <span className={classes.language}>
+          <div className={classes.sectionDesktop}>
+            <Tooltip title={t("change_language")} enterDelay={300}>
+              <Button
+                edge="end"
+                aria-controls={menuId}
+                aria-haspopup="true"
+                color="inherit"
+                onClick={handleProfileMenuOpen}
+              >
+                <TranslateIcon/>
+                <span className={classes.language}>
               {LANGUAGES_LABEL.filter((language) => {
                 return language.code === LANGUAGE;
               })[0].text}
               </span>
-              <ExpandMoreIcon fontSize="small"/>
-            </Button>
-          </Tooltip>
-          <Menu
-            anchorEl={anchorEl}
-            keepMounted
-            open={Boolean(anchorEl)}
-            onClose={handleClose}
-            PaperProps={{
-              style: {
-                maxHeight: ITEM_HEIGHT * 4.5,
-                width: "20ch",
-              },
-            }}
-          >
-            {LANGUAGES_LABEL.map((language) => (
-              <MenuItem key={language.code}
-                        component={Link}
-                        data-no-link="true"
-                        href={`?lng=${language.code}`}
-                        selected={language.code === LANGUAGE}
-                        lang={language.code}
-                        hrefLang={language.code}
-                        onClick={handleClose}>
-                {language.text}
-              </MenuItem>
-            ))}
-          </Menu>
-          <Tooltip title={t("sections.create-exam")} enterDelay={300}>
-            <IconButton
-              color="inherit"
-              component={Link}
+                <ExpandMoreIcon fontSize="small"/>
+              </Button>
+            </Tooltip>
 
-              href={"create-exam"}
-            >
-              <NoteAdd/>
-            </IconButton>
-          </Tooltip>
-          <Tooltip title={t("logout")} enterDelay={300}>
+            <Tooltip title={t("sections.create-exam")} enterDelay={300}>
+              <IconButton
+                color="inherit"
+                component={Link}
+
+                href={"create-exam"}
+              >
+                <NoteAdd/>
+              </IconButton>
+            </Tooltip>
+            <Tooltip title={t("logout")} enterDelay={300}>
+              <IconButton
+                color="inherit"
+                onClick={logout}
+              >
+                <ExitToApp/>
+              </IconButton>
+            </Tooltip>
+          </div>
+          <div className={classes.sectionMobile}>
             <IconButton
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
               color="inherit"
-              onClick={logout}
             >
-              <ExitToApp/>
+              <MoreIcon/>
             </IconButton>
-          </Tooltip>
+          </div>
         </Toolbar>
       </AppBar>
+      {renderMobileMenu}
+      {renderLanguageMenu}
     </div>
   );
 }
